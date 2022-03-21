f4b9364d54002103755235119c8f161c94edc1fa
ducmanh99 <pdm241099@gmail.com>
2022-03-21 15:44:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// home work 1
+// let person = {
+// 	name: "Bob",
+// 	occupation: "web developer",
+// 	hobbies: "painting",
+// };
+// // c1
+// console.log(person.name);
+// // c2
+// console.log(person["name"]);
+// // c3
+// let { hobbies } = person;
+// console.log(hobbies);
+// home work 2
+// const person = {
+// 	name: "Bob",
+// 	occupation: "web developer",
+// 	hobbies: "painting",
+// };
+// let a = Object.keys(person);
+// console.log(a.toString().toUpperCase());
+// home work 3
+let obj = {
+	foo: { a: "hello", b: "world" },
+	bar: ["example", "mem", null, { xyz: 6 }, 88],
+	qux: [4, 8, 12],
+};
